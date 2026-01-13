ab15abb8080edd758a844335cbe5af715ec60aa2
Samuel <samuelsgamer11@gmail.com>
2026-01-13 12:52:57 -0300

    Banco de dados Filmes

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE filmes (
+
+--     filme_id SERIAL PRIMARY KEY,
+--     titulo VARCHAR(100) NOT NULL,
+--     genero VARCHAR(50) NOT NULL,
+--     ano INTEGER NOT NULL,
+--     diretor VARCHAR(100),
+--     duracao_min INTEGER,
+--     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
---
+++ database/seeds.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO filmes (titulo, genero, ano, diretor, duracao_min) VALUES
+-- ('Vingadores: Ultimato', 'Ação', 2019, 'Anthony Russo', 181),
+-- ('Homem-Aranha: Sem Volta Para Casa', 'Ação', 2021, 'Jon Watts', 148),
+-- ('Interestelar', 'Ficção Científica', 2014, 'Christopher Nolan', 169),
+-- ('O Poderoso Chefão', 'Drama', 1972, 'Francis Ford Coppola', 175),
+-- ('Parasita', 'Suspense', 2019, 'Bong Joon-ho', 132),
+-- ('Matrix', 'Ficção Científica', 1999, 'Wachowski', 136),
+-- ('Cidade de Deus', 'Drama', 2002, 'Fernando Meirelles', 130);
