7422737e8d37f2b392682bdabe4d2acc3c6d85c1
jeanlucas2k07 <jeanlucas2k07@gmail.com>
2026-01-13 16:55:09 -0300

    Adicião de validação nas rotas

--- app/models/film.py
+++ app/models/film.py
@@ -1,10 +1,30 @@
-from pydantic import BaseModel
+from datetime import datetime
+from pydantic import (
+    BaseModel, 
+    Field,
+    field_validator 
+)
 
 class Film(BaseModel):
-    titulo: str
-    genero: str
-    diretor: str
-    
-    duracao_min: int
+    titulo: str = Field(min_length=1)
+    genero: str = Field(min_length=1)
     ano: int
+    diretor: str = Field(min_length=1)
+    duracao_min: int
     
+    @field_validator("titulo", "diretor", "genero")
+    @classmethod
+    def capitaliza_str(cls, value: str) -> str:
+        return value.capitalize()
+    
+    @field_validator("ano", "duracao_min")
+    @classmethod
+    def validate_int_filds(cls, value: int) -> int:
+        if value <= 0:
+            raise ValueError(f"O Titulo e Duração têm que ser maior que 0")
+        
+        return value
+
+class FilmResponse(Film):
+    filme_id: int
+    criado_em: datetime
