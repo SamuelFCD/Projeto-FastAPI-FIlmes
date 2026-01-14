62c5598ed90f3cc76ca59c1a49d2a63937948e78
Samuel <samuelsgamer11@gmail.com>
2026-01-14 12:55:40 -0300

    DataBase atualizado

--- app/database/schema.sql
+++ app/database/schema.sql
@@ -1,27 +1,11 @@
-CREATE TABLE autor (
-    autor_id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    data_nascimento DATE,
-    nacionalidade VARCHAR(50)
-);
 
-CREATE TABLE generos (
-    genero_id SERIAL PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL UNIQUE,
-    descricao TEXT
-);
 
 CREATE TABLE filmes (
     filme_id SERIAL PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
-    ano INTEGER NOT NULL,
-    duracao_min INTEGER,
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    autor_id INTEGER REFERENCES autor(autor_id)
-);
-
-CREATE TABLE filme_genero (
-    filme_id INTEGER REFERENCES filmes(filme_id),
-    genero_id INTEGER REFERENCES generos(genero_id),
-    PRIMARY KEY (filme_id, genero_id)
+    genero VARCHAR(50) NOT NULL,
+    ano INTEGER NOT NULL CHECK (ano > 0),
+    diretor VARCHAR(100) NOT NULL,
+    duracao_min INTEGER NOT NULL CHECK (duracao_min > 0),
+    criado_em TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
